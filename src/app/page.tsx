815e442c9d6565b88086c13e21408c1ae684b9b8
m-nwajha <m.nawjha@gmail.com>
2026-08-12 12:16:40 +0300

    Gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Offerings from "@/components/Offerings";
+import Gallery from "@/components/Gallery";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Offerings />
+      <Gallery />
     </>
   );
 }
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import { Container } from './ui/Container';
+
+const GALLERY_ITEMS = [
+  {
+    src: '/images/about-right-top.png',
+    alt: 'Shisha with rising smoke',
+  },
+  {
+    src: '/images/instagram-galerie-img1.jpg',
+    alt: 'Signature burger and fries',
+  },
+  {
+    src: '/images/gallery-img-9.jpg',
+    alt: 'Fine dining table setting',
+  },
+  {
+    src: '/images/about-right-botton.png',
+    alt: 'Dubai Lounge seating area',
+  },
+  {
+    src: '/images/unsere-geschichte-img2.png',
+    alt: 'Dubai Lounge seating area',
+  },
+  {
+    src: '/images/instagram-galerie-img3.jpg',
+    alt: 'Dubai Lounge seating area',
+  }
+];
+
+const Gallery = () => {
+  return (
+    <section id="gallery" className="overflow-x-hidden bg-[#0a0905] py-5">
+      <svg width="0" height="0" className="absolute" aria-hidden>
+        <defs>
+          <clipPath id="gallery-clip-path" clipPathUnits="objectBoundingBox">
+            <path d="M0.33,0 L0.92,0 Q1,0 0.9806,0.0776 L0.7694,0.9224 Q0.75,1 0.67,1 L0.08,1 Q0,1 0.0194,0.9224 L0.2306,0.0776 Q0.25,0 0.33,0 Z" />
+          </clipPath>
+        </defs>
+      </svg>
+      <Container className="w-full">
+        <Swiper
+          className="!overflow-visible"
+          spaceBetween={20}
+          grabCursor
+          slidesPerView={1.3}
+          breakpoints={{
+            480: { slidesPerView: 1.8 },
+            768: { slidesPerView: 2.4 },
+            1024: { slidesPerView: 4 },
+          }}
+        >
+          {GALLERY_ITEMS.map((item) => (
+            <SwiperSlide key={item.src}>
+              <div className="gallery-frame aspect-square">
+                <div className="gallery-frame__content">
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    fill
+                    sizes="(min-width: 1024px) 22vw, 60vw"
+                    className="object-cover"
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
+    </section>
+  );
+};
+
+export default Gallery;
